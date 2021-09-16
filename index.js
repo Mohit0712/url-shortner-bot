9154d43c17693f3619f55e17efd8c409d0163bf3
theAbdulKader <abdulkadernow@gmail.com>
2021-09-17 02:32:44 +0600

    Added encoded url in api request

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
-const regex = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/
+const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
 require("dotenv").config();
 const bot = new Telegraf(process.env.bot_api_key)
 
@@ -15,16 +15,24 @@ bot.command('help', (ctx) => {
   ctx.replyWithHTML("<em>📤 Send me any valid URL( with http:// or https:// ) and I will give you a <b>Short Url</b> that you can share with anyone. Example-\n\n<code>https://something.com/xyz/something</code></em>");
 });
 
+
+// api_key = Jb-IOE5Bye086pAY
 bot.on('message', async(ctx) => {
     var message = ctx.message.text
     if (message == undefined) {
         ctx.replyWithHTML("<em>❗You are sending file. Send a valid Link to Shorten it.</em>")
     } else if (!regex.test(message)) {
-        ctx.replyWithHTML("<em>You are sending an ⚠️ invalid link kindly recheck and send again with http:// or https://</em>")
+        ctx.replyWithHTML("<em>Regex You are sending an ⚠️ invalid link kindly recheck and send again with http:// or https://</em>")
     } else {
+      var encodedUrl = Buffer.from(message).toString('base64');
         var config = {
             method: 'get',
-            url: `${process.env.api_url}/create?api_key=${process.env.api_key}&u=${message}`
+            url: `${process.env.api_url}/create?api_key=${process.env.api_key}&u=${encodedUrl}&encoded=true`
+          // method: 'post',
+          // url: "https://akurls.ml/url",
+        //   data: {
+       //      url: `${message}`
+       //    }
         };
         axios(config)
             .then(async function(response) {
@@ -33,7 +41,7 @@ bot.on('message', async(ctx) => {
                     ctx.replyWithHTML(`<em>✔️ Url Shortened Successfully shortened Url (Tap to Copy):</em>\n\n <code>${data.shortUrl}</code>`)
                     
                 } else if (data.error && data.error == "invalid link") {
-                    ctx.replyWithHTML("<em>❗ You are sending an invalid link kindly recheck and send again with http:// or https://</em>")
+                    ctx.replyWithHTML("<em>❗ Data You are sending an invalid link kindly recheck and send again with http:// or https://</em>")
                 } else {
                     ctx.replyWithHTML("<em>Try again</em>")
                 }
@@ -52,6 +60,6 @@ bot.on('message', async(ctx) => {
 
 bot.launch();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT);
